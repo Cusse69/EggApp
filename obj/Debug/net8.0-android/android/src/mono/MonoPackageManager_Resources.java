55e27f27457da680935afb506c231520dddf037d
Cusse69 <marcusnordand@gmail.com>
2024-03-10 23:37:50 +0100

    gut app yees

--- obj/Debug/net8.0-android/android/src/mono/MonoPackageManager_Resources.java
+++ obj/Debug/net8.0-android/android/src/mono/MonoPackageManager_Resources.java
@@ -3,6 +3,7 @@ public class MonoPackageManager_Resources {
 	public static String[] Assemblies = new String[]{
 		/* We need to ensure that "EggApp.dll" comes first in this list. */
 		"EggApp.dll",
+		"CommunityToolkit.Mvvm.dll",
 		"GoogleGson.dll",
 		"Microsoft.Extensions.Configuration.dll",
 		"Microsoft.Extensions.Configuration.Abstractions.dll",
@@ -19,6 +20,9 @@ public class MonoPackageManager_Resources {
 		"Microsoft.Maui.dll",
 		"Microsoft.Maui.Essentials.dll",
 		"Microsoft.Maui.Graphics.dll",
+		"Syncfusion.Licensing.dll",
+		"Syncfusion.Maui.Core.dll",
+		"Syncfusion.Maui.Sliders.dll",
 		"Xamarin.Android.Glide.dll",
 		"Xamarin.Android.Glide.Annotations.dll",
 		"Xamarin.Android.Glide.DiskLruCache.dll",
